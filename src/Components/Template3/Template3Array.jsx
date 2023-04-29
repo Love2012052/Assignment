4eec85655dab59574b819dc21fabb45fd6f5b82e
Love <love2012052@akgec.ac.in>
2023-04-29 17:33:46 +0530

    second commit

--- src/Components/Template3/Template3Array.jsx
+++ src/Components/Template3/Template3Array.jsx
@@ -1,12 +1,12 @@
 const Template3Array = [
   {
-    icon: "bi bi-person-fill",
+    icon: "bi bi-person-fill-gear",
     topic: "Management",
     description:
       "Supervisor/ Front-line manager/ Senior Manager/ Board of Director/ CEO/ President of the Organization",
   },
   {
-    icon: "bi bi-person-fill-gear",
+    icon: "bi bi-person-fill",
     topic: "Engineering",
     description:
       "Students can start working to build a better future and can apply for ME/ M.Tech/ M.S.",
